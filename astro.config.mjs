66fbbe9b0a3c81216ac947942b478ec6e5e76400
will-thomson4 <will.thomson4@gmail.com>
2024-12-26 18:44:28 +0000

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  site: 'https://will-thomson4.github.io',
+  integrations: [react(), tailwind()],
 });
