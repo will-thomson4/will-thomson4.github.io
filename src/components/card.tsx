f14b27772734a53b517d53dc71bbf4c8db932f2f
will-thomson4 <will.thomson4@gmail.com>
2025-02-02 12:21:18 +0000

    Update button spacing

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -15,15 +15,15 @@ const Card = ({
       <div className="flex flex-col gap-2 border-t border-gray-300 bg-gray-100 p-4">
         <h2 className="text-base font-medium border-b w-fit pr-8">{title}</h2>
 
-        <div className="flex gap-8 text-xs">
+        <div className="flex flex-col sm:flex-row gap-2 text-xs">
           <button
-            className="bg-gray-800 hover:bg-gray-900 text-white py-1 px-3 rounded-full"
+            className="bg-gray-800 hover:bg-gray-900 text-white py-1 px-3 rounded-full w-fit"
             onClick={() => animationStart(title)}
           >
             Start
           </button>
           <button
-            className="bg-gray-100 hover:bg-gray-200 text-gray-800 py-1 px-3 rounded-full border border-gray-400"
+            className="bg-gray-100 hover:bg-gray-200 text-gray-800 py-1 px-3 rounded-full border border-gray-400 w-fit"
             onClick={() => animationEnd(title)}
           >
             Finish
